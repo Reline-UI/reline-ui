eeb97fd27ccffd83f5d80299607d7e1c85dfd0b8
Debajoyti Paul <learn.ishan2001@gmail.com>
2026-02-25 21:21:50 +0530

    Navigation menu for the whole website has been added

--- example/main.jsx
+++ example/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "../src/index.css";
 import { RelineThemeProvider } from "../src";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RelineThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </RelineThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
